17b34b35c7d04b515d4c95b7541d0007d43b54e0
Andrew Doyle <adoyle1215@gmail.com>
2022-08-11 17:36:43 -0700

    Linting

--- simple_linear_regression/demo.py
+++ simple_linear_regression/demo.py
@@ -1,13 +1,22 @@
-## Necessary imports
+'''This stand-alone script accompanies a blog post available here:
+https://doylead.me/?p=19
+
+The goal of this script is to generate data which follow a basic linear relationship and to show
+two different methods of calculating the parameters for the best linear fit.  We show that an
+approach based on the dot product in an abstract vector space matches trusted results from
+scikit-learn
+'''
+
+# Necessary imports
 import numpy as np
 from sklearn import linear_model
 from tabulate import tabulate
 import pylab as plt
 
-## For repeatable results
+# For repeatable results
 np.random.seed(42)
 
-## Construct the data
+# Construct the data
 gen_m = np.random.rand()
 gen_b = np.random.rand()
 x = np.arange(0,21).reshape(-1, 1)
@@ -15,46 +24,41 @@ x = np.arange(0,21).reshape(-1, 1)
 # Includes some random noise around each point
 y = gen_m * x + gen_b + np.random.rand(*x.shape)
 
-## Fit a linear model using sklearn
+# Fit a linear model using sklearn
 reg = linear_model.LinearRegression()
 reg.fit(x, y)
 
-# We extract scalar values from the return format
-# scikit-learn provides
+# We extract scalar values from the return format scikit-learn provides
 skl_m = reg.coef_[0][0]
 skl_b = reg.intercept_[0]
 
-## Fit a model using our techniques (specifically
-## using Equation (6) and abstract approaches)
-## We need the transpose to keep inner
-## dimensions consistent
+# Fit a model using our techniques (specifically using Equation (6) and abstract approaches).  We
+#  need the transpose to keep inner dimensions consistent
 x_mean = np.average(x)
 y_mean = np.average(y)
 x_T = np.transpose(x)
 
-# The output of both dot products is a 1x1 matrix.
-# We use array[0][0] to extract the first/only
+# The output of both dot products is a 1x1 matrix. We use array[0][0] to extract the first/only
 # value of this matrix as a scalar
 afit_m = np.dot(x_T-x_mean, y-y_mean)[0][0] / \
     np.dot(x_T-x_mean, x-x_mean)[0][0]
 afit_b = y_mean - afit_m*x_mean
 afit_y = afit_m * x + afit_b
 
-## Visualize results
 # Compare parameters from scikit-learn and
 # our methods
 table_data = [
     ["scikit-learn", skl_m, skl_b],
     ["analytical", afit_m, afit_b]
 ]
-print(tabulate(table_data, 
+print(tabulate(table_data,
     headers=["Method", "Slope", "Intercept"]))
 
 # Make a plot for the data
-plt.plot(x, y, 
+plt.plot(x, y,
     ms=15,
-    lw=0, 
-    marker='o', 
+    lw=0,
+    marker='o',
     color='blue',
     label="Sample Data")
 
